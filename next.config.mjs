f36552c67c723d0e7a4f02968b1bca4ef5063d2d
Trey Hoover <treyhoover@gmail.com>
2024-07-04 19:28:30 +0100

    Initialize

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	reactStrictMode: true,
+	experimental: {
+		reactCompiler: true,
+		ppr: true,
+	},
+};
+
+export default nextConfig;
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+	return (
+		<div className="font-sans min-h-screen p-8">
+			<h1>Shelter Scout</h1>
+		</div>
+	);
+}
